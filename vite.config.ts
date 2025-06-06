d4ade4a3bfaf1ec17f1738693bc4583bdecc6f23
MahmoudSamyDev <softwareengineer.mahmoud.samy@gmail.com>
2025-06-06 10:07:30 +0300

    Using alias import for better developer experience

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import { visualizer } from 'rollup-plugin-visualizer';
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), visualizer()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   build: {
     rollupOptions: {
       output: {
